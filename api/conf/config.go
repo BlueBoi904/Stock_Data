74bfb41cac9809cd8787ce91d2ffc92c612e4524
Joseph Collins <jcollins@totalwine.com>
2020-06-18 11:43:52 -0400

    viper config with toml

---
+++ api/conf/config.go
@@ -0,0 +1,51 @@
+package conf
+
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+type InternalConfig struct {
+	v *viper.Viper
+}
+
+//GetString  returns value of desired key
+func (i *InternalConfig) GetString(key string) string {
+	return i.v.GetString(key)
+}
+
+//PrintValues Util to log all Vipers values
+func (i *InternalConfig) PrintValues() {
+	for _, k := range i.v.AllKeys() {
+		fmt.Printf("%s = %s\n", k, i.v.GetString(k))
+	}
+}
+
+//Initialize config starting with the base path
+func Initialize(path string) *InternalConfig {
+	v := viper.New()
+	v.AddConfigPath(path)
+	v.SetConfigName("base")
+	v.AutomaticEnv()
+
+	err := v.MergeInConfig()
+	if err != nil {
+		fmt.Println("Error With Config")
+	}
+	env := v.GetString("ENV")
+
+	if env != "" {
+		v.SetConfigName(env)
+		err = v.MergeInConfig()
+		if err != nil {
+			fmt.Println("Error Merging Config")
+		}
+	}
+
+	config := InternalConfig{
+		v: v,
+	}
+
+	return &config
+}
--- api/main.go
+++ api/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/Stock_Data/api/conf"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -11,6 +13,12 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+
+	config := conf.Initialize("./conf")
+	apiKey := config.GetString("apiKey")
+
+	fmt.Printf("Api Key:%v", apiKey)
+
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }

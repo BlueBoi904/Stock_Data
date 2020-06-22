7430efe5a6d19ccf3c96cb3d15c8f5210a15df3a
BlueBoi904 <cliff.mirschel@gmail.com>
2020-06-22 11:41:25 -0700

    Moved quote logic

--- api/main.go
+++ api/main.go
@@ -14,26 +14,31 @@ func homepage(w http.ResponseWriter, r *http.Request) {
 	_, _ = fmt.Fprintf(w, "Homepage endpoint hit!")
 }
 
-func handleRequests() {
-	http.HandleFunc("/", homepage)
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-func main() {
+func quote(w http.ResponseWriter, r *http.Request) {
+	_, _ = fmt.Fprintf(w, "Quote page hit!")
 	config := conf.Initialize("./conf")
 	apiKey := config.GetString("apiKey")
 
-	fmt.Printf("Api Key:%v", apiKey)
 	finnhubClient := finnhub.NewAPIClient(finnhub.NewConfiguration()).DefaultApi
 	auth := context.WithValue(context.Background(), finnhub.ContextAPIKey, finnhub.APIKey{
 		Key: apiKey,
 	})
-
+	// Get quote on APPL
 	quote, _, err := finnhubClient.Quote(auth, "AAPL")
 	fmt.Printf("%+v\n", quote)
 	if err != nil {
 		panic(err)
 	}
+
 	fmt.Printf("Quote: %v", quote.C)
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homepage)
+	http.HandleFunc("/quote", quote)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func main() {
 	handleRequests()
 }

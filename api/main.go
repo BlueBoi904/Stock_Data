505089c04e9cfa28b66dd07d2a194afd4b1eea23
BlueBoi904 <cliff.mirschel@gmail.com>
2020-06-22 10:46:25 -0700

    Added basic finnhub api client and tested basic quote call

--- api/main.go
+++ api/main.go
@@ -1,24 +1,39 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"github.com/Finnhub-Stock-API/finnhub-go"
+	"github.com/Stock_Data/api/conf"
 	"log"
 	"net/http"
-
-	"github.com/Stock_Data/api/conf"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+
+func homepage(w http.ResponseWriter, r *http.Request) {
+	_, _ = fmt.Fprintf(w, "Homepage endpoint hit!")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homepage)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func main() {
-
 	config := conf.Initialize("./conf")
 	apiKey := config.GetString("apiKey")
 
 	fmt.Printf("Api Key:%v", apiKey)
+	finnhubClient := finnhub.NewAPIClient(finnhub.NewConfiguration()).DefaultApi
+	auth := context.WithValue(context.Background(), finnhub.ContextAPIKey, finnhub.APIKey{
+		Key: apiKey,
+	})
 
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	quote, _, err := finnhubClient.Quote(auth, "AAPL")
+	fmt.Printf("%+v\n", quote)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Quote: %v", quote.C)
+	handleRequests()
 }

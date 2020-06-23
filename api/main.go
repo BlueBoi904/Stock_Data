cf7510b0d638a436875f917a01b4d1aaff5f99b9
BlueBoi904 <cliff.mirschel@gmail.com>
2020-06-22 17:49:33 -0700

    Added news route and comments

--- api/main.go
+++ api/main.go
@@ -11,30 +11,39 @@ import (
 	"github.com/Stock_Data/api/conf"
 )
 
+var config = conf.Initialize("./conf")
+var apiKey = config.GetString("apiKey")
+var finnhubClient = finnhub.NewAPIClient(finnhub.NewConfiguration()).DefaultApi
+var auth = context.WithValue(context.Background(), finnhub.ContextAPIKey, finnhub.APIKey{
+	Key: apiKey,
+})
+
 func homepage(w http.ResponseWriter, r *http.Request) {
 	_, _ = fmt.Fprintf(w, "Homepage endpoint hit!")
 }
 
-func quote(w http.ResponseWriter, r *http.Request) {
-	config := conf.Initialize("./conf")
-	apiKey := config.GetString("apiKey")
-
-	finnhubClient := finnhub.NewAPIClient(finnhub.NewConfiguration()).DefaultApi
-	auth := context.WithValue(context.Background(), finnhub.ContextAPIKey, finnhub.APIKey{
-		Key: apiKey,
-	})
-	// Get quote on APPL
-	quote, _, err := finnhubClient.Quote(auth, "AAPL")
-	fmt.Printf("%+v\n", quote)
+func news(w http.ResponseWriter, r *http.Request) {
+	// Get general market news
+	generalNews, _, err := finnhubClient.GeneralNews(auth, "general", nil)
 	if err != nil {
 		panic(err)
 	}
+	json.NewEncoder(w).Encode(generalNews)
+}
+func quote(w http.ResponseWriter, r *http.Request) {
+	// Get price data for a ticker
+	quote, _, err := finnhubClient.Quote(auth, "AAPL")
+	if err != nil {
+		panic(err)
+	}
+
 	json.NewEncoder(w).Encode(quote)
 }
 
 func handleRequests() {
 	http.HandleFunc("/", homepage)
 	http.HandleFunc("/quote", quote)
+	http.HandleFunc("/news", news)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 

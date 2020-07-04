8ae1c500cec9d2dc2e8a60b83a86abfcce0667cc
Joseph Collins <jcollins@totalwine.com>
2020-07-04 19:29:42 -0400

    Husky

---
+++ client/.huskyrc.js
@@ -0,0 +1,12 @@
+const tasks = arr => arr.join(' && ');
+
+module.exports = {
+  hooks: {
+    'pre-commit': tasks([
+      'yarn lint-staged',
+      'yarn lint',
+      // 'yarn audit',
+      // 'yarn test',
+    ]),
+  },
+};

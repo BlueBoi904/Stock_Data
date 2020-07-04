0e21d824048a7bf56225aa96249a844eb5081477
BlueBoi904 <cliff.mirschel@gmail.com>
2020-07-04 15:38:53 -0700

    Added folder structure and basic pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,23 @@
-import React  from 'react';
-import './App.css';
-import {Subscriber} from './subscription/Subscriber'
+import React from "react";
+import "./App.css";
+import { Subscriber } from "./subscription/Subscriber";
+import News from "./routes/News/News";
+import Quote from "./routes/Quote/Quote";
 
-
-const sub = Subscriber.get({id: "test"})
-const obj = {id: "Socket 1"}
+const sub = Subscriber.get({ id: "test" });
+const obj = { id: "Socket 1" };
 
 sub.stream.subscribe({
   next: (message) => {
-      console.log(`Message From Here`,message)
-  }
-})
-
-
+    console.log(`Message From Here`, message);
+  },
+});
 
 function App() {
-
   return (
-    <div className="App">
-      
-    <div>Something Bad</div>
-
-
+    <div>
+      <News />
+      <Quote />
     </div>
   );
 }

1d182d92014ed8f036caa358e87b751b358a7230
Joseph Collins <jcollins@totalwine.com>
2020-07-04 22:14:15 -0400

    text

--- client/src/components/atoms/Text.tsx
+++ client/src/components/atoms/Text.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import './styles/Text.scss';
 
 export function TextTitle({ children }: { children: string }) {
-  return <div className="Title H1">{children}</div>;
+  return <h1 className="Title H1">{children}</h1>;
 }
 
 export function TextSubTitle({ children }: { children: string }) {
-  return <div className="Title H2">{children}</div>;
+  return <h2 className="Title H2">{children}</h2>;
+}
+
+export function TextBody({ children }: { children: string }) {
+  return <div className="Title body">{children}</div>;
 }

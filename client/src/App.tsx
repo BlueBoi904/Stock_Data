f14ee36ca3b30ae2a4a324fa00572ae2fda61f22
BlueBoi904 <cliff.mirschel@gmail.com>
2020-07-10 21:13:11 -0700

    added searching for ticker

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,30 +5,13 @@ import Quote from './routes/Quote/Quote';
 import { Home } from './routes/Home/Home';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 
-/*
-const SubToTest = SubscribeToPath('test');
-const SubToOther = SubscribeToPath('other');
-
-SubToTest.addListener({
-  next: (message) => {
-    console.log('Subscribed to Test', message);
-  },
-});
-
-SubToOther.addListener({
-  next: (message) => {
-    console.log('Subscribed to OTher', message);
-  },
-});
-*/
-
 function App(): JSX.Element {
   return (
     <Router>
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/news/:ticker" component={News} />
-        <Route path="/quote" exact component={Quote} />
+        <Route path="/quote/:ticker" exact component={Quote} />
       </Switch>
     </Router>
   );
--- client/src/routes/Quote/Quote.tsx
+++ client/src/routes/Quote/Quote.tsx
@@ -2,18 +2,6 @@ import { BaseQuoteTemplate } from './templates';
 import React from 'react';
 
 function Quote(): JSX.Element {
-  /*
-  async function getQuote() {
-    try {
-      const response = await fetch('http://localhost:8080/quote');
-      const data = await response.json();
-      console.log('data', data);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-  */
-
   return <BaseQuoteTemplate />;
 }
 

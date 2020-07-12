8fca08812462a669ae9e46c999db28cdf1ecaaa6
Joseph Collins <jcollins@totalwine.com>
2020-07-11 22:09:35 -0400

    Added Statistical Ticker Data cache

---
+++ client/src/hooks/useStatisticalData.ts
@@ -0,0 +1,16 @@
+import { Cache, useCache } from '../cache/Cache';
+import { API } from '../api';
+
+export const StatisticalCache = new Cache<string, { [key: string]: string }>({
+  async load(key) {
+    const { data } = await API.get('/statistical', {
+      ticker: key,
+    });
+    return data;
+  },
+});
+
+export function useStatisticalData(ticker: string) {
+  const asyncValue = useCache(StatisticalCache, ticker);
+  return { ...asyncValue, ticker };
+}
